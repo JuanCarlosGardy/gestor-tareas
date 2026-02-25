1f3a3d637403fbfa475e5a94a91fe807956557de
JuanCarlosGardy <juancarlosgardy6@gmail.com>
2026-02-26 00:09:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -113,9 +113,6 @@ import {
   serverTimestamp,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import {
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 const auth = window.firebaseAuth;
 const db = window.firebaseDB;
 
